9f065e625dd928451233f618197f3abfb57a9a5e
PATOUOMBELINE <48788316+PATOUOMBELINE@users.noreply.github.com>
2019-03-22 11:18:26 +0100

    Add files via upload

---
+++ formationswingOMBELINE/src/main/java/Controller/PersonBean.java
@@ -0,0 +1,174 @@
+package Controller;
+
+import com.sun.rowset.JdbcRowSetImpl;
+import domain.Person;
+
+import javax.sql.rowset.JdbcRowSet;
+import java.sql.SQLException;
+
+public class PersonBean {
+    static final String JDBC_DRIVER =
+            "com.mysql.jdbc.Driver";
+    static final String DB_URL =
+            "jdbc:mysql://localhost:3306/formation";
+    static final String DB_USER = "root";
+    static final String DB_PASS = "root";
+    private JdbcRowSet rowSet = null;
+
+    public PersonBean() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            rowSet = new JdbcRowSetImpl();
+            rowSet.setUrl(DB_URL);
+            rowSet.setUsername(DB_USER);
+            rowSet.setPassword(DB_PASS);
+            rowSet.setCommand("SELECT * FROM Person");
+            rowSet.execute();
+
+        }catch(SQLException | ClassNotFoundException ex){
+                ex.printStackTrace();
+            }
+        }
+        public Person create (Person p){
+            try {
+                rowSet.moveToInsertRow();
+                //rowSet.updateInt("personId", p.getPersonId());  // car id en auto increment
+                rowSet.updateString("firstName", p.getFirstName());
+                rowSet.updateString("middleName", p.getMiddleName());
+                rowSet.updateString("lastName", p.getLastName());
+                rowSet.updateString("email", p.getEmail());
+                rowSet.updateString("phone", p.getPhone());
+                rowSet.insertRow();
+                rowSet.moveToCurrentRow();
+            } catch (SQLException ex) {
+                try {
+                    rowSet.rollback();
+                    p = null;
+                } catch (SQLException e) {
+
+                }
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public Person update (Person p){
+            try {
+                rowSet.updateString("firstName", p.getFirstName());
+                rowSet.updateString("middleName", p.getMiddleName());
+                rowSet.updateString("lastName", p.getLastName());
+                rowSet.updateString("email", p.getEmail());
+                rowSet.updateString("phone", p.getPhone());
+                rowSet.updateRow();
+                rowSet.moveToCurrentRow();
+            } catch (SQLException ex) {
+                try {
+                    rowSet.rollback();
+                } catch (SQLException e) {
+
+                }
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public void delete () {
+            try {
+                rowSet.moveToCurrentRow();
+                rowSet.deleteRow();
+            } catch (SQLException ex) {
+                try {
+                    rowSet.rollback();
+                } catch (SQLException e) {
+                }
+                ex.printStackTrace();
+            }
+
+        }
+
+        public Person moveFirst () {
+            Person p = new Person();
+            try {
+                rowSet.first();
+                p.setPersonId(rowSet.getInt("personId"));
+                p.setFirstName(rowSet.getString("firstName"));
+                p.setMiddleName(rowSet.getString("middleName"));
+                p.setLastName(rowSet.getString("lastName"));
+                p.setEmail(rowSet.getString("email"));
+                p.setPhone(rowSet.getString("phone"));
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public Person moveLast () {
+            Person p = new Person();
+            try {
+                rowSet.last();
+                p.setPersonId(rowSet.getInt("personId"));
+                p.setFirstName(rowSet.getString("firstName"));
+                p.setMiddleName(rowSet.getString("middleName"));
+                p.setLastName(rowSet.getString("lastName"));
+                p.setEmail(rowSet.getString("email"));
+                p.setPhone(rowSet.getString("phone"));
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public Person moveNext () {
+            Person p = new Person();
+            try {
+                if (rowSet.next() == false)
+                    rowSet.previous();
+                p.setPersonId(rowSet.getInt("personId"));
+                p.setFirstName(rowSet.getString("firstName"));
+                p.setMiddleName(rowSet.getString("middleName"));
+                p.setLastName(rowSet.getString("lastName"));
+                p.setEmail(rowSet.getString("email"));
+                p.setPhone(rowSet.getString("phone"));
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public Person movePrevious () {
+            Person p = new Person();
+            try {
+                if (rowSet.previous() == false)
+                    rowSet.next();
+                p.setPersonId(rowSet.getInt("personId"));
+                p.setFirstName(rowSet.getString("firstName"));
+                p.setMiddleName(rowSet.getString("middleName"));
+                p.setLastName(rowSet.getString("lastName"));
+                p.setEmail(rowSet.getString("email"));
+                p.setPhone(rowSet.getString("phone"));
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return p;
+        }
+
+        public Person getCurrent () {
+            Person p = new Person();
+            try {
+                rowSet.moveToCurrentRow();
+                p.setPersonId(rowSet.getInt("personId"));
+                p.setFirstName(rowSet.getString("firstName"));
+                p.setMiddleName(rowSet.getString("middleName"));
+                p.setLastName(rowSet.getString("lastName"));
+                p.setEmail(rowSet.getString("email"));
+                p.setPhone(rowSet.getString("phone"));
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            return p;
+        }
+    }
---
+++ formationswingOMBELINE/src/main/java/fr/formation/lille/swing/App.java
@@ -0,0 +1,26 @@
+package fr.formation.lille.swing;
+
+import UI.PersonUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Hello Lille,Whatsup (::)!
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+        f.getContentPane().add(new PersonUI());
+        f.setSize(800, 380);
+        f.setVisible(true);
+
+    }
+
+
+
+}
